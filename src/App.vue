1ad75b60a5d3491e66ae05a02f86dc908450fcc7
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2018-11-09 12:37:39 +0300

    AUTH-44 adding base project infrastructure based on vue cli 3

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <center>
+      <img alt="Vue logo" src="./assets/logo.png">
+    </center>
+    <AuthForm/>
+  </div>
+</template>
+
+<script>
+import AuthForm from './components/AuthForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AuthForm,
+  },
+};
+</script>
+
+<style lang="scss">
+@import 'assets/styles/base.scss';
+</style>
