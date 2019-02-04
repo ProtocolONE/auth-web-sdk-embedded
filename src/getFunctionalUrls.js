a237427a7f01df9709175a9eb3d5a536146d4063
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-02-04 16:01:10 +0300

    launching the app with https AUTH-13

--- src/getFunctionalUrls.js
+++ src/getFunctionalUrls.js
@@ -6,7 +6,7 @@ export default function getFunctionalUrls(apiUrl = 'https://auth1.eu.gamenet.ru'
     apiUrl,
     apiCreateOrderUrl: `${apiUrl}/api/v1/order`,
     apiGetProjectPackagesUrl: `${apiUrl}/api/v1/project/package`,
-    devAuthFormUrl: 'http://localhost:4040/',
+    devAuthFormUrl: 'https://localhost:4040/',
     getAuthFormUrl({ clientID, redirectUri }) {
       if (process.env.NODE_ENV === 'development') {
         return this.devAuthFormUrl;
